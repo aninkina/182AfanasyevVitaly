cbdeb3255a8429cd817491150865dbad5258ad47
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 17:54:55 +0300

    Task11

---
+++ HW_1/Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое слово:");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Введите второе слово:");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("Введите третье слово:");
+            string word3 = Console.ReadLine();
+            Console.WriteLine("-{0}-\n-{1}-\n-{2}-", word1, word2, word3);
+            Console.ReadKey();
+        }
+    }
+}
