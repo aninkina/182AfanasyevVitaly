f6886dbaf54403d56a8392de3e8adaa2912e52a6
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 17:52:56 +0300

    Task10

---
+++ HW_1/Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое слово:");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Введите второе слово:");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("Введите третье слово:");
+            string word3 = Console.ReadLine();
+            Console.WriteLine("{0}!{1}!{2}", word1, word2, word3);
+            Console.ReadKey();
+        }
+    }
+}
