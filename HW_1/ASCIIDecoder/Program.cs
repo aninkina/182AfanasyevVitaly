1a5edc8aee27d2f35a78330d9b7e6c9e020c11aa
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 16:52:13 +0300

    Task3

---
+++ HW_1/ASCIIDecoder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ASCIIDecoder
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите код символа (целое число от 32 до 127):");
+            int Code = int.Parse(Console.ReadLine());
+            Console.WriteLine("Символ в таблице ASCII:");
+            Console.WriteLine((char)Code);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW_1/ASCIIDecoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ASCIIDecoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ASCIIDecoder")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("518f9923-ae8c-43f6-a3f5-b60a0041329e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
