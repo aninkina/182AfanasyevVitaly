ff6eb887ddc73f46155624868a90bc068b78d478
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 17:36:00 +0300

    Task05

--- HW_1/Task5/Program.cs
+++ HW_1/Task5/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Введите длину первого катета:");
+            string cathString1 = Console.ReadLine();
+            Console.WriteLine("Введите длину второго катета:");
+            string cathString2 = Console.ReadLine();
+            double cath1, cath2;
+            if (!double.TryParse(cathString1, out cath1) || !double.TryParse(cathString2, out cath2))
+            {
+                Console.WriteLine("Ошибка! Неверный формат входных данных!\nВведите вещественные числа(разделитель - запятая)!");
+                Console.ReadKey();
+                return;
+            }
+            double hyp = Math.Sqrt(cath1 * cath1 + cath2 * cath2);
+            Console.WriteLine("Длина гипотенузы = " + hyp.ToString());
+            Console.ReadKey();
         }
     }
 }
