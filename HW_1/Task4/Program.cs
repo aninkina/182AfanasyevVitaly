ae06d67fb03ce656bc07a8b9c688568231128c1e
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 17:41:48 +0300

    Task04

--- HW_1/Task4/Program.cs
+++ HW_1/Task4/Program.cs
@@ -11,14 +11,20 @@ namespace ASCIIDecoder
             string uString = Console.ReadLine();
             Console.WriteLine("Введите R(сопротивление):");
             string rString = Console.ReadLine();
-            float U, R;
-            if (!float.TryParse(uString, out U) || !float.TryParse(rString, out R))
+            double U, R;
+            if (!double.TryParse(uString, out U) || !double.TryParse(rString, out R))
             {
                 Console.WriteLine("Ошибка! Неверный формат входных данных!\nВведите вещественные числа(разделитель - запятая)!");
                 Console.ReadKey();
                 return;
             }
-            float I = U / R, P = U * U / R;
+            if (R == 0)
+            {
+                Console.WriteLine("Ошибка! Деление на ноль!");
+                Console.ReadKey();
+                return;
+            }
+            double I = U / R, P = U * U / R;
             Console.WriteLine("I(сила тока) = {0}, P(мощность) = {1}", I, P);
             Console.ReadKey();
         }
