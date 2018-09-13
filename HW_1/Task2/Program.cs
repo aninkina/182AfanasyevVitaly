27393aebb9a5937ac436f9d3e9751788fa332cb2
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-13 16:31:02 +0300

    Task2

---
+++ HW_1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(" *   ***   *  * *  *   ** *   *** ** ");
+            Console.WriteLine("* * * * * * * * * * * *   **  *   * *");
+            Console.WriteLine("***  ***  *** *** *** *   * * **  ** ");
+            Console.WriteLine("* *   *   * * * * * * *   * * *   * *");
+            Console.WriteLine("* *   *   * * * * * *  ** **  *** ** ");
+            Console.ReadKey();
+        }
+    }
+}
