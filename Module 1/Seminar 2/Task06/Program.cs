51e2850c1dd485414d289e40fc77ceaad028bf17
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-23 13:55:28 +0300

    HW_2

---
+++ Module 1/Seminar 2/Task06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task06
+{
+    struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    class Program
+    {
+        static double InputDouble(string input, double minValue = double.MinValue, double maxValue = double.MaxValue)
+        {
+            Console.WriteLine("Enter " + input + ": ");
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result) || (result < minValue) || (result > maxValue))
+            {
+                Console.WriteLine("Invalid input format! Try again");
+            }
+            return result;
+        }
+
+        static string PointLocation(Point p, double r = 10.0)
+        {
+            return p.X * p.X + p.Y * p.Y > r * r ? "outside" : "inside";
+        }
+
+        static void Main()
+        {
+            do
+            { 
+                Point p = new Point();
+                p.X = InputDouble("X coordinate of point");
+                p.Y = InputDouble("Y coordinate of point");
+                Console.WriteLine("Point is {0} the circle", PointLocation(p));
+                Console.WriteLine("Press ESC to exit. Press any other key to continue");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
