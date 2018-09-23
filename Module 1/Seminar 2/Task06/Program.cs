15b7eb34fc822a32c288d1d912b965473422fe9b
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-23 16:42:08 +0300

    HW_2

--- Module 1/Seminar 2/Task06/Program.cs
+++ Module 1/Seminar 2/Task06/Program.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 
+/*
+   Дисциплина: "Программирование"
+   Группа: БПИ182_1
+   Студент: Афанасьев Виталий Олегович
+   Задача: 
+           Задан круг с центром в начале координат и радиусом R = 10.
+           Ввести координаты точки и вывести сообщение:
+           "Точка внутри круга!" или "Точка вне круга!".
+           Предусмотреть проверку входных данных и цикл повторения решения.
+*/
+
 namespace Task06
 {
     struct Point
@@ -34,7 +45,7 @@ namespace Task06
                 Point p = new Point();
                 p.X = InputDouble("X coordinate of point");
                 p.Y = InputDouble("Y coordinate of point");
-                Console.WriteLine("Point is {0} the circle", PointLocation(p, R));
+                Console.WriteLine("Point is {0} the circle!", PointLocation(p, R));
                 Console.WriteLine("Press ESC to exit. Press any other key to continue");
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
