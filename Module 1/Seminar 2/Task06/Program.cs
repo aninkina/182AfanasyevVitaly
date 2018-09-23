848b6febf1f71eaccd33ca53fa115601f2065a6b
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-23 14:02:27 +0300

    HW_2

--- Module 1/Seminar 2/Task06/Program.cs
+++ Module 1/Seminar 2/Task06/Program.cs
@@ -21,19 +21,20 @@ namespace Task06
             return result;
         }
 
-        static string PointLocation(Point p, double r = 10.0)
+        static string PointLocation(Point p, double R)
         {
-            return p.X * p.X + p.Y * p.Y > r * r ? "outside" : "inside";
+            return p.X * p.X + p.Y * p.Y > R * R ? "outside" : "inside";
         }
 
         static void Main()
         {
             do
-            { 
+            {
+                const double R = 10;
                 Point p = new Point();
                 p.X = InputDouble("X coordinate of point");
                 p.Y = InputDouble("Y coordinate of point");
-                Console.WriteLine("Point is {0} the circle", PointLocation(p));
+                Console.WriteLine("Point is {0} the circle", PointLocation(p, R));
                 Console.WriteLine("Press ESC to exit. Press any other key to continue");
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
