dbb0629b3dd2b43c4b8309d90e05f68cf28d0b8e
Vitaly Afanasyev <voafanasev@edu.hse.ru>
2018-09-23 14:17:15 +0300

    HW_2

---
+++ Module 1/HW_2/Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static double InputDouble(string input, double minValue = double.MinValue, double maxValue = double.MaxValue)
+        {
+            Console.WriteLine("Enter " + input + ": ");
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result) || (result < minValue) || (result > maxValue))
+            {
+                Console.WriteLine("Invalid input format! Try again");
+            }
+            return result;
+        }
+
+        static double F(double x)
+        {
+            double x2 = x * x;
+            double x3 = x2 * x;
+            double x4 = x3 * x;
+            return 12 * x4 + 9 * x3 - 3 * x2 + 2 * x - 4;
+        }
+
+        static void Main()
+        {
+            do
+            {
+                double x = InputDouble("X");
+                Console.WriteLine($"F(X) = {F(x)}");
+                Console.WriteLine("Press ESC to exit. Press any other key to continue");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
